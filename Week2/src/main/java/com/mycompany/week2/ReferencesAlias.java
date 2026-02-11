8a9078cf0c3c4c2385d95c96ec1a9ed75a3d672a
doomertheboomer <hackerdoomer@gmail.com>
2026-02-11 11:01:15 +0700

    convert week2 to netbeans proj

--- Week2/src/main/java/com/mycompany/week2/ReferencesAlias.java
+++ Week2/src/main/java/com/mycompany/week2/ReferencesAlias.java
@@ -1,3 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.week2;
+
+/**
+ *
+ * @author sph
+ */
 public class ReferencesAlias {
 
     static class Student {
